a70afe769f08e1b7b1ba3e7147f77da6ecbc61dd
udayateja <udaya.kolli@innovacx.com>
2019-01-25 22:27:14 +0530

    casorel changes

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent implements OnInit {
-//test
+//test123
   constructor() { }
 
   ngOnInit() {
